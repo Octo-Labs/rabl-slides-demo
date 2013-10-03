64a29489a2ef3d6109d7979988bd9bd1d6d13a04
Jeremy Green <jeremy@octolabs.com>
2013-10-02 23:57:13 -0500

    Inintial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsSlidesDemo::Application.config.secret_key_base = '48379e3d6616d0780df0bf7b19f1717de564e4b65f998937a6d716a1d79f9188521b9bd1873ca95c0a0c297ebdfaf1e8a7b72c9162400af0a2427ef22f83749f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsSlidesDemo::Application.config.session_store :cookie_store, key: '_rails-slides-demo_session'
